bf8ac048dcb00bb499c9bb99a8336f8effcd76ff
enriquetagersol <quecagersol@gmail.com>
2024-05-24 11:39:49 +0200

    Create registrarEmpresa.js

---
+++ Lambda/registrarEmpresa.js
@@ -0,0 +1,53 @@
+const AWS = require('aws-sdk');
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event) => {
+    // Establece los encabezados de CORS para la respuesta
+    const headers = {
+        "Access-Control-Allow-Origin": "*", 
+        "Access-Control-Allow-Credentials": true,
+        "Content-Type": "application/json"
+    };
+
+    if (!event.body) {
+        console.log("El event.body es null o undefined");
+        return {
+            statusCode: 400,
+            headers: headers,
+            body: JSON.stringify({ message: "El event.body es null o undefined" })
+        };
+    }
+
+    try {
+        let requestBody = JSON.parse(event.body);
+        const empresa_id = requestBody.empresa_id;
+        const telf = requestBody.telf;
+        const email = requestBody.email;
+        const nombre_empresa = requestBody.nombre;
+
+        let item = {
+            TableName: "Admins",
+            Item: {
+                "ADMIN_ID": empresa_id, 
+                "nombre": nombre_empresa,
+                "email": email,
+                "telf": telf
+            }
+        };
+
+        await dynamoDB.put(item).promise();
+
+        return {
+            statusCode: 200,
+            headers: headers,
+            body: JSON.stringify({ message: "Datos almacenados correctamente" })
+        };
+    } catch (error) {
+        console.error("Error en la función Lambda:", error);
+        return {
+            statusCode: 500,
+            headers: headers,
+            body: JSON.stringify({ message: "Error al procesar la solicitud", error: error.message })
+        };
+    }
+};
