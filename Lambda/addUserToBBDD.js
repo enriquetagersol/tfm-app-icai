3aee80aa9b4474dfb03c13a8d380b99e8e7d9eec
enriquetagersol <quecagersol@gmail.com>
2024-05-24 11:44:42 +0200

    Create addUserToBBDD.js

---
+++ Lambda/addUserToBBDD.js
@@ -0,0 +1,70 @@
+const AWS = require('aws-sdk');
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event) => {
+    
+    console.log("Evento recibido:", event);
+    try {
+        
+        let cognitoUser = {};
+        const properties = ['email', 'sub', 'name', 'family_name', 'phone_number', 'username', 'isAdmin', 'empresa'];
+
+        properties.forEach(prop => {
+            if (event.hasOwnProperty(prop)) {
+                cognitoUser[prop] = event[prop];
+            } else {
+                console.log(`Propiedad ${prop} no encontrada en el evento.`);
+            }
+        });
+        let item;
+        
+        if(!cognitoUser.empresa)
+        {
+
+            item = {
+                TableName: "Users",
+                Item: {
+                    "USER_ID": cognitoUser.sub, 
+                    "email": cognitoUser.email,
+                    "first_name": cognitoUser.name,
+                    "last_name": cognitoUser.family_name,
+                    "IS_ADMIN": cognitoUser.isAdmin,
+                    "phone": cognitoUser.phone_number,
+                    "username": cognitoUser.username
+     
+                }
+            }
+        }else{
+            item = {
+                TableName: "Users",
+                Item: {
+                    "USER_ID": cognitoUser.sub, 
+                    "email": cognitoUser.email,
+                    "first_name": cognitoUser.name,
+                    "last_name": cognitoUser.family_name,
+                    "IS_ADMIN": cognitoUser.isAdmin,
+                    "phone": cognitoUser.phone_number,
+                    "username": cognitoUser.username,
+                    "empresa": cognitoUser.empresa
+     
+                }
+            }
+            
+        }
+        //};
+
+        await dynamoDB.put(item).promise();
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify('Usuario agregado a la base de datos')
+        };
+    } catch (error) {
+        console.error("Error:", error);
+        console.error("Error:", error.message);
+        return {
+            statusCode: 500,
+            body: JSON.stringify('Error al procesar la solicitud')
+        };
+    }
+};
