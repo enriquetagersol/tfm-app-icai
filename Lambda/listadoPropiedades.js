861d6acf0fb148a9f5df9d76f6ac4331e62cc492
enriquetagersol <quecagersol@gmail.com>
2024-05-24 11:49:38 +0200

    Create listadoPropiedades.js

---
+++ Lambda/listadoPropiedades.js
@@ -0,0 +1,49 @@
+const AWS = require('aws-sdk');
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event) => {
+    const requestBody = JSON.parse(event.body);
+    const estate_id = requestBody.estate_id;
+    
+    if (!estate_id) {
+        return {
+            statusCode: 400,
+            headers: {"Access-Control-Allow-Origin": "*"},
+            body: JSON.stringify({ message: "El campo 'estate_id' está vacío o no se proporcionó." })
+        };
+    }
+
+    const paramsProperties = {
+        TableName: "Properties",
+        IndexName: "Estate",
+        KeyConditionExpression: "Estate = :estate_id",
+        ExpressionAttributeValues: {":estate_id": estate_id}
+    };
+
+    try {
+        const propertiesData = await dynamoDB.query(paramsProperties).promise();
+        const properties = propertiesData.Items;
+
+            return {
+                statusCode: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*", // Para producción especifica el dominio en lugar de usar *
+                    "Access-Control-Allow-Credentials": true, // Si es necesario para tus credenciales
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(properties)
+            };
+    } catch (error) {
+        console.error("Error al consultar DynamoDB: ", error);
+        return {
+            statusCode: 500,
+            headers: {
+                "Access-Control-Allow-Origin": "*", // Para producción especifica el dominio en lugar de usar *
+                "Access-Control-Allow-Credentials": true, // Si es necesario para tus credenciales
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ message: "Error al procesar la solicitud", error: error.message })
+        };
+    }
+};
+
