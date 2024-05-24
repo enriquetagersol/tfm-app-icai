1d1ba94bd2a3bb7fd45af33ae1a3ee0291810b89
enriquetagersol <quecagersol@gmail.com>
2024-05-24 10:54:35 +0200

    Create listadoFincas.js

---
+++ Lambda/listadoFincas.js
@@ -0,0 +1,41 @@
+const AWS = require('aws-sdk');
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event) => {
+
+    const requestBody = JSON.parse(event.body);
+    const adminUserId = requestBody.admin_user_id;
+
+    const params = {
+        TableName: "Estates",
+        IndexName: "admin_user_id", //GSI en DynamoDB
+        KeyConditionExpression: "admin_user_id = :adminUserId",
+        ExpressionAttributeValues: {
+            ":adminUserId": adminUserId
+        }
+    };
+
+    try {
+        const data = await dynamoDB.query(params).promise();
+        return {
+            statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Origin": "*", 
+                "Access-Control-Allow-Credentials": true, 
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data.Items)
+        };
+    } catch (error) {
+        console.error("Error al consultar DynamoDB: ", error);
+        return {
+            statusCode: 500,
+            headers: {
+                "Access-Control-Allow-Origin": "*", 
+                "Access-Control-Allow-Credentials": true,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ message: "Error al procesar la solicitud", error: error.message })
+        };
+    }
+};
