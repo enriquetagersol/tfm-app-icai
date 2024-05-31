b175a8f7c70aec25ce4990254e9b9ff3d33ba122
enriquetagersol <quecagersol@gmail.com>
2024-05-31 08:35:31 +0200

    Update borrarMsg.js

--- Lambda/borrarMsg.js
+++ Lambda/borrarMsg.js
@@ -1,150 +1,65 @@
+//ARN --> arn:aws:lambda:eu-west-3:699928454448:function:borrarMsg
+//Region --> eu-west-3 (París)
+
+//Esta función se emplea para eliminar los mensajes del usuario al administrador
+//Modifica el atributo 'Estado' de la tabla de DynamoDB
+
 const AWS = require('aws-sdk');
-const dynamodb = new AWS.DynamoDB.DocumentClient();
-const ses = new AWS.SES({ region: 'eu-west-3' });
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
-    let body;
-    if (event.body) {
-        body = JSON.parse(event.body); 
-    } else {
-        console.error('No se recibió un cuerpo válido');
+    const data = JSON.parse(event.body);
+    const suggestionId = data.id;
+
+    if (!suggestionId) {
         return {
             statusCode: 400,
             headers: {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Credentials": true,
-                "Content-Type": "application/json"
+                "Access-Control-Allow-Origin": "*", 
+                "Access-Control-Allow-Credentials": true, 
+                "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+                "Access-Control-Allow-Methods": "OPTIONS,GET,PUT,POST,DELETE" // Métodos permitidos
             },
-            body: 'No se recibió un cuerpo válido'
+            body: JSON.stringify({ message: "El campo 'id' está vacío o no se proporcionó." })
         };
     }
 
-    const userId = body.user_id;
-    const finca_id = body.finca_id;
-    const prop_id = body.prop_id;
-    const asunto = body.asunto;
-    const mensaje = body.mensaje;
-    const admin_id = body.admin_id;
-
-    // Buscar el email del administrador en la tabla users
-    const getUserParams = {
-        TableName: 'Users',
+    const params = {
+        TableName: "Sugerencias",
         Key: {
-            'USER_ID': admin_id
-        }
+            "SUGESTION_ID": suggestionId
+        },
+        UpdateExpression: "set Estado = :estado",
+        ExpressionAttributeValues: {
+            ":estado": "Eliminado"
+        },
+        ReturnValues: "UPDATED_NEW"
     };
 
     try {
-        const userResponse = await dynamodb.get(getUserParams).promise();
-        const adminUser = userResponse.Item;
-
-        if (!adminUser) {
-            return {
-                statusCode: 404,
-                headers: {
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Credentials": true,
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({ message: 'Administrador no encontrado.' })
-            };
-        }
-
-        const adminEmail = adminUser.email;
-
-        // Realizar la consulta para contar sugerencias existentes
-        const paramsQuery = {
-            TableName: "Sugerencias",
-            IndexName: "Finca_id", // GSI en DynamoDB
-            KeyConditionExpression: "Finca_id = :Finca_id",
-            ExpressionAttributeValues: {
-                ":Finca_id": finca_id
-            }
-        };
-
-        const data = await dynamodb.query(paramsQuery).promise();
-        var n = data.Count + 1;
-        var sugestion_id = n.toString() + '-' + finca_id;
-        const dateForDynamo = getCurrentFormattedDate();
-
-        // Crear el nuevo ítem en la tabla Sugerencias
-        let item = {
-            TableName: "Sugerencias",
-            Item: {
-                "SUGESTION_ID": sugestion_id, 
-                "Mensaje": mensaje,
-                "Asunto": asunto,
-                "Finca_id": finca_id,
-                "Prop_id": prop_id,
-                "User_id": userId,
-                "Estado": "Pendiente",
-                "Fecha": dateForDynamo,
-                "Admin_id": admin_id
-            }
-        };
-
-        await dynamodb.put(item).promise();
-        console.log("Sugerencia almacenada con éxito con ID:", sugestion_id);
-        // Enviar email al administrador
-        await sendAdminEmail(adminEmail, asunto, mensaje);
-        
-
+        await dynamoDB.update(params).promise();
         return {
             statusCode: 200,
             headers: {
                 "Access-Control-Allow-Origin": "*", 
                 "Access-Control-Allow-Credentials": true, 
-                "Content-Type": "application/json"
+                "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+                "Access-Control-Allow-Methods": "OPTIONS,GET,PUT,POST,DELETE" // Métodos permitidos
             },
-            body: JSON.stringify({ message: "Sugerencia almacenada con éxito"})
+            body: JSON.stringify({ message: "El estado se actualizó correctamente a 'Eliminado'." })
         };
-
     } catch (error) {
-        console.error("Error al procesar la solicitud:", error);
+        console.error("Error al actualizar el estado: ", error);
         return {
             statusCode: 500,
             headers: {
-                "Access-Control-Allow-Origin": "*", 
-                "Access-Control-Allow-Credentials": true, 
-                "Content-Type": "application/json"
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": true,
+                "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+                "Access-Control-Allow-Methods": "OPTIONS,GET,PUT,POST,DELETE" // Métodos permitidos
             },
-            body: JSON.stringify({
-                message: "Error al procesar la solicitud",
-                error: error.message
-            })
+            body: JSON.stringify({ message: "Error al procesar la solicitud", error: error.message })
         };
-    }
-};
-
-const getCurrentFormattedDate = () => {
-    const now = new Date();
-    const year = now.getUTCFullYear();
-    const month = (now.getUTCMonth() + 1).toString().padStart(2, '0');
-    const day = now.getUTCDate().toString().padStart(2, '0');
-    const hours = now.getUTCHours().toString().padStart(2, '0');
-    const minutes = now.getUTCMinutes().toString().padStart(2, '0');
-
-    return `${year}${month}${day}T${hours}${minutes}`;
-};
-
-const sendAdminEmail = async (email, subject, message) => {
-    const params = {
-        Source: 'gestionfincas.tfm@gmail.com',
-        Destination: { ToAddresses: [email] },
-        Message: {
-            Subject: { Data: `Notificación de mensaje` },
-            Body: {
-                Text: { Data: `Ha recibido un nuevo mensaje.\n\nAsunto: ${subject}\nMensaje: ${message} \n\nPuede ver el detalle accediendo a la plataforma` }
-            }
-        }
-    };
-
-    try {
-        await ses.sendEmail(params).promise();
-        console.log(`Correo enviado a ${email}`);
-    } catch (error) {
-        console.error(`Error al enviar el correo a ${email}:`, error);
-        throw new Error(`Error al enviar el correo a ${email}`);
     }
 };
 
