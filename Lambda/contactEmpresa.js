b9b392f8d903282934801d4162bdbbb4246bec13
enriquetagersol <quecagersol@gmail.com>
2024-06-02 16:01:59 +0200

    Update contactEmpresa.js

--- Lambda/contactEmpresa.js
+++ Lambda/contactEmpresa.js
@@ -42,7 +42,7 @@ exports.handler = async (event) => {
       const dataAdmin = await dynamoDB.get(params).promise();
       const email_admin = dataAdmin.Item.email;
       
-      let boundary = "NextPart";
+      /*let boundary = "NextPart";
       
       // Encabezados del mensaje
       let rawEmailMessage = `From: gestionfincas.tfm@gmail.com\r\n`;
@@ -61,14 +61,91 @@ exports.handler = async (event) => {
       rawEmailMessage += `<h3>${asunto}</h3>\r\n`;
       rawEmailMessage += `<p>${msg}</p>\r\n`;
       rawEmailMessage += "</body>\r\n</html>\r\n";
+      rawEmailMessage += "\r\n";*/
+      
+      let boundary = "NextPart";
+    
+      // Encabezados del mensaje
+      let rawEmailMessage = `From: gestionfincas.tfm@gmail.com\r\n`;
+      rawEmailMessage += `To: ${email_admin}\r\n`;
+      rawEmailMessage += `Subject: Notificación de mensaje\r\n`;
+      rawEmailMessage += "MIME-Version: 1.0\r\n";
+      rawEmailMessage += `Content-Type: multipart/mixed; boundary="${boundary}"\r\n`;
       rawEmailMessage += "\r\n";
+      
+      // Cuerpo del mensaje en HTML
+      rawEmailMessage += `--${boundary}\r\n`;
+      rawEmailMessage += "Content-Type: text/html; charset=UTF-8\r\n";
+      rawEmailMessage += "Content-Transfer-Encoding: 7bit\r\n";
+      rawEmailMessage += "\r\n";
+      rawEmailMessage += `
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <style>
+          body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+          }
+          .container {
+            width: 100%;
+            max-width: 600px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #ffffff;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+          }
+          .header {
+            background-color: #FFA600;
+            color: white;
+            padding: 20px;
+            text-align: center;
+            border-top-left-radius: 10px;
+            border-top-right-radius: 10px;
+          }
+          .content {
+            padding: 20px;
+          }
+          .footer {
+            background-color: #FFA600;
+            color: white;
+            text-align: center;
+            padding: 10px;
+            border-bottom-left-radius: 10px;
+            border-bottom-right-radius: 10px;
+          }
+          .content p {
+            margin: 10px 0;
+          }
+        </style>
+      </head>
+      <body>
+        <div class="container">
+          <div class="header">
+            <h1>Notificación de mensaje</h1>
+          </div>
+          <div class="content">
+            <p><strong>Asunto:</strong> ${asunto}</p>
+            <p><strong>Mensaje:</strong> ${msg}</p>
+          </div>
+          <div class="footer">
+            <p>© 2024 tfm-app-icai.</p>
+          </div>
+        </div>
+      </body>
+      </html>
+      `;
+      rawEmailMessage += "\r\n";
+      rawEmailMessage += `--${boundary}--\r\n`;
       
       // Configurar los parámetros para sendRawEmail
       const params1 = {
         RawMessage: { Data: rawEmailMessage }
       };
       
-      // Envía el correo electrónico con Amazon SES utilizando sendRawEmail
       try {
         const sendEmailResponse = await ses.sendRawEmail(params1).promise();
         console.log('Correo enviado correctamente.');
@@ -101,4 +178,5 @@ exports.handler = async (event) => {
     }
   
 };
+
 
