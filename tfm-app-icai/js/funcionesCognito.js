f5af19626a18f62c97f0c76c97e266f84be6c6a8
enriquetagersol <quecagersol@gmail.com>
2024-06-17 12:30:56 +0200

    Update funcionesCognito.js

--- tfm-app-icai/js/funcionesCognito.js
+++ tfm-app-icai/js/funcionesCognito.js
@@ -282,8 +282,8 @@ function inviteUser(){
     AWS.config.update({
 	    region: region, 
 	    credentials: new AWS.Credentials({
-		    accessKeyId: 'AKIA2F5X4UUYB42FUJQC',
-		    secretAccessKey: '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'
+		    accessKeyId: 'AKIA2F5X4UUYBAWQKDEE',// Nueva: AKIA2F5X4UUYBAWQKDEE Antigua: AKIA2F5X4UUYB42FUJQC
+		    secretAccessKey: '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'//Nueva: YnN/MM6NJAgN/sxtnmaSYmi4QlNlZgit5gXexArN Antigua: InDm8+Tgii5VvNkfoP2IrzQYbX+S7Jl6eSaQli8o
 	    })
 	});
 	
@@ -387,8 +387,8 @@ function changeTempPass(){
 	AWS.config.update({
 	    region: region,
 	    credentials: new AWS.Credentials({
-		    accessKeyId: 'AKIA2F5X4UUYB42FUJQC',
-		    secretAccessKey: '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'
+		    accessKeyId: 'AKIA2F5X4UUYBAWQKDEE', // Nueva: AKIA2F5X4UUYBAWQKDEE Antigua: AKIA2F5X4UUYB42FUJQC
+		    secretAccessKey: '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' //Nueva: YnN/MM6NJAgN/sxtnmaSYmi4QlNlZgit5gXexArN Antigua: InDm8+Tgii5VvNkfoP2IrzQYbX+S7Jl6eSaQli8o
 	    })
 	});
 
